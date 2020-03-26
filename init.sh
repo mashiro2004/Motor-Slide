8d12f037e13729e179562ae1f86f7687d26d7d4f
mashiro2004 <52611512+mashiro2004@users.noreply.github.com>
2020-03-26 21:10:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 echo 15 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio15/direction
+echo "out" > /sys/class/gpio/gpio15/direction
 echo 23 > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio23/direction
 echo 24 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio24/direction
+echo "out" > /sys/class/gpio/gpio24/direction
 echo 17 > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio17/direction
 echo 27 > /sys/class/gpio/export
